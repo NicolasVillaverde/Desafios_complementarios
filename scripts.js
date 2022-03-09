6a387621b474d12f9fe7dc9f6b027d07f2387381
Nicolas Villaverde <villaverdenicolaspablo@gmail.com>
2022-03-09 18:14:34 -0300

    Desafio incorporando eventos

--- scripts.js
+++ scripts.js
@@ -1,9 +1,19 @@
 console.log(`Desafio Tema: To do App`);
 
 const botonAgregar = document.getElementById(`btn__agregar`);
+botonAgregar.addEventListener(`click`, agregarTarea);
 
-botonAgregar.addEventListener(`click`, () => {
-    const input = document.getElementById(`input`);
+//Permite ingresar valores pulsando la tecla `Enter`
+document.addEventListener("keypress", (e) => {
+    if (e.key === `Enter`) {
+        agregarTarea();
+    }
+});
+
+const botonReset = document.getElementById(`btn__reset`);
+botonReset.addEventListener(`click`, eliminarTodo);
+
+function agregarTarea() {
     const nuevaTarea = input.value;
 
     if (nuevaTarea != "") {
@@ -13,6 +23,7 @@ botonAgregar.addEventListener(`click`, () => {
         nuevoSpan.innerText = nuevaTarea;
 
         root.appendChild(nuevoDiv);
+        nuevoDiv.appendChild(tacharActividad());
         nuevoDiv.appendChild(nuevoSpan);
         nuevoDiv.className += "tarjeta__tareas d-flex justify-content-between align-items-center p-4 m-1 rounded-3";
         nuevoDiv.appendChild(botonEliminar());
@@ -20,7 +31,7 @@ botonAgregar.addEventListener(`click`, () => {
     } else {
         alert("Por favor ingrese una tarea");
     }
-});
+}
 
 function botonEliminar() {
     const nuevoBtn = document.createElement(`button`);
@@ -33,4 +44,28 @@ function botonEliminar() {
         root.removeChild(divAEliminar);
     });
     return nuevoBtn;
+}
+
+function eliminarTodo() {
+    const arrtodoAEliminar = document.querySelectorAll(`div.tarjeta__tareas`);
+
+    for (let i = 0; i < arrtodoAEliminar.length; ++i) {
+        arrtodoAEliminar[i].remove();
+    }
+}
+
+function tacharActividad() {
+    const tilde = document.createElement(`input`);
+    tilde.setAttribute("type", "checkbox");
+
+    tilde.addEventListener(`change`, (e) => {
+        const spanAModificar = tilde.parentNode.querySelector("span");
+        if (e.target.checked) {
+            spanAModificar.className += "text-decoration-line-through fst-italic";
+            console.log(spanAModificar);
+        } else {
+            spanAModificar.className = ``;
+        }
+    });
+    return tilde;
 }
